9873e6ac84fd947766c392b4b25c2466561ec5fc
thanos <koutroubas.thanos@gmail.com>
2021-02-15 01:58:34 +0200

    Added query 1 SQL,parquet, changed csv conv

--- convert_csv_to_parquet.py
+++ convert_csv_to_parquet.py
@@ -1,18 +1,56 @@
 from pyspark.sql import SparkSession
+from pyspark.sql.types import StructType, StructField
+from pyspark.sql.types import DoubleType, IntegerType, StringType, TimestampType
 
 spark = SparkSession.builder.appName("CSV2Parquet").getOrCreate()
 
 hdfs_path = "hdfs://master:9000/movie_data/"
 
-# Read CSV to Dataframes
-movies = spark.read.format("csv").option("header", "false").load(hdfs_path + "movies.csv")
-movie_genres = spark.read.format("csv").option("header", "false").load(hdfs_path + "movie_genres.csv")
-ratings = spark.read.format("csv").option("header", "false").load(hdfs_path + "ratings.csv")
+# Create schema for Table Movies
+schema_movies = StructType([
+    StructField("movieId", IntegerType()),
+    StructField("title", StringType()),
+    StructField("summary", StringType()),
+    StructField("timestamp", TimestampType()),
+    StructField("duration", DoubleType()),
+    StructField("cost", IntegerType()),
+    StructField("revenue", IntegerType()),
+    StructField("popularity", DoubleType()),
+])
 
-# Show spark dataframes
-#df.show(n=2)
+schema_movie_genres = StructType([
+    StructField("movieId", IntegerType()),
+    StructField("genre", StringType()),
+])
+
+schema_ratings = StructType([
+    StructField("userId", IntegerType()),
+    StructField("movieId", IntegerType()),
+    StructField("rating", DoubleType()),
+    StructField("timestamp", StringType()),
+])
+
+
+# Read CSV to Dataframes
+df_movies = spark.read \
+    .format("csv") \
+    .option("header", "false") \
+    .schema(schema_movies) \
+    .load(hdfs_path + "movies.csv")
+
+df_movie_genres = spark.read \
+    .format("csv") \
+    .option("header", "false") \
+    .schema(schema_movie_genres) \
+    .load(hdfs_path + "movie_genres.csv")
+
+df_ratings = spark.read \
+    .format("csv") \
+    .option("header", "false") \
+    .schema(schema_ratings) \
+    .load(hdfs_path + "ratings.csv")
 
 # Save as parquet files
-movies.write.parquet(hdfs_path + "movies.parquet")
-movie_genres.write.parquet(hdfs_path + "movie_genres.parquet")
-ratings.write.parquet(hdfs_path + "ratings.parquet")
+df_movies.write.parquet(hdfs_path + "movies.parquet")
+df_movie_genres.write.parquet(hdfs_path + "movie_genres.parquet")
+df_ratings.write.parquet(hdfs_path + "ratings.parquet")
