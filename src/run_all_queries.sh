3de3a5d59425d83dd39e82c387b007329edc06f8
thanos <koutroubas.thanos@gmail.com>
2021-02-25 22:50:02 +0200

    Changed project directories structure

--- src/run_all_queries.sh
+++ src/run_all_queries.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 # Log Directory
-log_dir="logs"
+log_dir="output"
+code_dir="code"
 
 if [ ! -d "${log_dir}" ]; then
     mkdir ${log_dir}
@@ -11,8 +12,9 @@ fi
 rm -f "queries_exec_times.txt"
 
 # Run all queries in Spark and save logs
-for python_file in query_*.py; do
-    echo "Executing: spark-submit ${python_file} > ${log_dir}/log_${python_file}.txt 2>&1"
-    spark-submit ${python_file} > "${log_dir}/log_${python_file}.txt" 2>&1
+for python_file in ${code_dir}/query_*.py; do
+    file_name=`echo $python_file | awk '{split($0,a,"/"); print a[2]}'`
+    echo "Executing: spark-submit ${python_file} > ${log_dir}/log_${file_name}.txt 2>&1"
+    spark-submit ${python_file} > "${log_dir}/log_${file_name}.txt" 2>&1
 done
 
